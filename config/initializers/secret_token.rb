11c7580ad2b48d8b9642d7bb6fc5121c062df1cc
anromeo <anthonykzullo@gmail.com>
2013-06-05 12:57:04 -0700

    created README.rdoc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetaTank::Application.config.secret_token = 'cc5492be8fc1e4719b0a8ec8cb3288d442f9fa83cf7a652a5de8df4885e3f72fa7d70ca88753fab7032b0e2b993eb7c92b6b341f91de2c4566bd2857afd3cf31'
