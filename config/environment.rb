0244986ac924e6f7b277049fe5e530db5b450c02
anromeo <anthonykzullo@gmail.com>
2013-06-10 21:11:14 -0700

    changed time format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 BetaTank::Application.initialize!
+
+Time::DATE_FORMATS[:posted_time] = "Posted at %b %d, %Y"
+
+Time::DATE_FORMATS[:updated_time] = "Last Updated at %b %d, %Y"
